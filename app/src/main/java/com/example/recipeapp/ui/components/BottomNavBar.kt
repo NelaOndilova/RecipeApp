3db2dbe30d672e9b5002c66c02ba7a9c6015a16f
NelaOndilova <nela.ondilova@student.spsmb.cz>
2026-03-05 20:05:16 +0000

    added icons to bottom navigation bar

--- app/src/main/java/com/example/recipeapp/ui/components/BottomNavBar.kt
+++ app/src/main/java/com/example/recipeapp/ui/components/BottomNavBar.kt
@@ -3,6 +3,12 @@ package com.example.recipeapp.ui.components
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 
 @Composable
 fun BottomBar(navController: NavController) {
@@ -13,28 +19,36 @@ fun BottomBar(navController: NavController) {
             selected = false,
             onClick = { navController.navigate("home") },
             label = { Text("Home") },
-            icon = {}
+            icon = {
+                Icon(Icons.Filled.Home, contentDescription = "Home")
+            }
         )
 
         NavigationBarItem(
             selected = false,
             onClick = { navController.navigate("addRecipe") },
             label = { Text("Add Recipe") },
-            icon = {}
+            icon = {
+                Icon(Icons.Filled.Add, contentDescription = "Add recipe")
+            }
         )
 
         NavigationBarItem(
             selected = false,
             onClick = { navController.navigate("favorites") },
             label = { Text("Favorites") },
-            icon = {}
+            icon = {
+                Icon(Icons.Filled.Favorite, contentDescription = "Favorite")
+            }
         )
 
         NavigationBarItem(
             selected = false,
             onClick = { navController.navigate("profile") },
             label = { Text("Profile") },
-            icon = {}
+            icon = {
+                Icon(Icons.Filled.Person, contentDescription = "Profile")
+            }
         )
     }
 }
