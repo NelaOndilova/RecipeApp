0bed2fde14d00d378d951ca30c8b1acf07143348
NelaOndilova <nela.ondilova@student.spsmb.cz>
2026-03-06 11:43:28 +0000

    added colors to bottom navigation bar

--- app/src/main/java/com/example/recipeapp/ui/components/BottomNavBar.kt
+++ app/src/main/java/com/example/recipeapp/ui/components/BottomNavBar.kt
@@ -9,45 +9,45 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
-
+import androidx.compose.ui.graphics.Color
 @Composable
 fun BottomBar(navController: NavController) {
 
-    NavigationBar {
+    NavigationBar (containerColor = Color(0xFFFFCAD4)){
 
         NavigationBarItem(
-            selected = false,
+            selected =  false,
             onClick = { navController.navigate("home") },
-            label = { Text("Home") },
+            label = { Text("Home", color = Color(0xFF822D3A)) },
             icon = {
-                Icon(Icons.Filled.Home, contentDescription = "Home")
+                Icon(Icons.Filled.Home, contentDescription = "Home", tint = Color(0xFFF08495))
             }
         )
 
         NavigationBarItem(
             selected = false,
             onClick = { navController.navigate("addRecipe") },
-            label = { Text("Add Recipe") },
+            label = { Text("Add Recipe", color = Color(0xFF822D3A)) },
             icon = {
-                Icon(Icons.Filled.Add, contentDescription = "Add recipe")
+                Icon(Icons.Filled.Add, contentDescription = "Add recipe", tint = Color(0xFFF08495))
             }
         )
 
         NavigationBarItem(
             selected = false,
             onClick = { navController.navigate("favorites") },
-            label = { Text("Favorites") },
+            label = { Text("Favorites", color = Color(0xFF822D3A)) },
             icon = {
-                Icon(Icons.Filled.Favorite, contentDescription = "Favorite")
+                Icon(Icons.Filled.Favorite, contentDescription = "Favorite", tint = Color(0xFFF08495))
             }
         )
 
         NavigationBarItem(
             selected = false,
             onClick = { navController.navigate("profile") },
-            label = { Text("Profile") },
+            label = { Text("Profile", color = Color(0xFF822D3A)) },
             icon = {
-                Icon(Icons.Filled.Person, contentDescription = "Profile")
+                Icon(Icons.Filled.Person, contentDescription = "Profile", tint = Color(0xFFF08495))
             }
         )
     }
